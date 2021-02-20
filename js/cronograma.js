88f0e356b7da69b204c373a352cf6019838ecf0a
AnnieFL <79063060+AnnieFL@users.noreply.github.com>
2021-02-20 20:38:28 -0300

    Add files via upload

--- js/cronograma.js
+++ js/cronograma.js
@@ -1,3 +1,22 @@
+//navbar
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+});
+
+//dropdown
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {
+        coverTrigger: false
+    });
+});
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.datepicker');
     var instances = M.Datepicker.init(elems, {
